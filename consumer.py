bfa9792d04d447fc60023b2b6f1168498fdb4a58
Aman Kumar Soni <aman-kumar.soni@scio.technology>
2020-06-11 11:32:38 +0530

    rabbitmq consumer

---
+++ consumer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials('guest', 'guest')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost', port='5672',virtual_host='/',credentials=pika.PlainCredentials('guest', 'guest')))
+channel = connection.channel()
+
+channel.queue_declare(queue='web-scrapper')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+
+
+
+channel.basic_consume(
+    queue='web-scrapper', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
